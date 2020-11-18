182beb68f75c27feb242dc01a3a30ff5e8443aeb
Chng-Zhi-Xuan <zhixuan374@gmail.com>
2020-11-18 16:46:11 +0800

    Refactor routes.js

--- routes.js
+++ routes.js
@@ -1,16 +1,16 @@
 const asyncHandler = require('express-async-handler');
+const constants = require('./Utils/constants');
 const router = require('express').Router();
 
+const familyMemberController = require('./Controllers/familyMemberController');
 const grantController = require('./Controllers/grantController');
 const householdController = require('./Controllers/householdController');
-const familyMemberController = require('./Controllers/familyMemberController');
 
 router.get('/', function(request, response) {
-    response.json({
-        message: 'Homepage of Grant Disbursement API'
-    });
+    response.status(constants.STATUS_OK).send('Homepage of Grant Disbursement API');
 });
 
+// Household APIs
 router.route('/household')
         .get(asyncHandler(householdController.getAllHouseholds))
         .post(asyncHandler(householdController.addHousehold));
@@ -18,10 +18,12 @@ router.route('/household')
 router.route('/household/:household_id')
         .get(asyncHandler(householdController.getHousehold));
 
+// Family Member APIs
 router.route('/familyMember')
         .get(asyncHandler(familyMemberController.getAllFamilyMembers))
         .post(asyncHandler(familyMemberController.addFamilyMember));
 
+// Grant APIs
 router.route('/studentEncouragementBonus')
         .get(asyncHandler(grantController.getStudentEncouragementBonusRecipients));
 
